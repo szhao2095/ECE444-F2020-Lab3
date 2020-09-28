9f6ff8c91b2b78fd355b12a4b7a3c05b7abeb98f
unknown <altair.lugia@gmail.com>
2020-09-28 16:41:12 -0400

    Finished activity 2 and 3.

--- README.md
+++ README.md
@@ -4,15 +4,27 @@ Flask - "Simple web form"
 This is a clone of: https://github.com/miguelgrinberg/flasky
 
 [act1ss]: images/Activity1.png "Activity 1 screenshot"
-[act2ss]: images/Activity2.png "Activity 2 screenshot"
+[act2ss1]: images/Activity2_1.png "Activity 2 screenshot 1"
+[act2ss2]: images/Activity2_2.png "Activity 2 screenshot 2"
+[act2ss3]: images/Activity2_3.png "Activity 2 screenshot 3"
+[act2ss4]: images/Activity2_4.png "Activity 2 screenshot 4"
 
 # Activity 1
 ![alt text][act1ss]
 
 # Activity 2
-![alt text][act2ss]
+![alt text][act2ss1]
+![alt text][act2ss2]
+![alt text][act2ss3]
+![alt text][act2ss4]
 
 # Activity 3
 **Briefly summarize the difference between SQL or NoSQL database.**
 
+SQL databases are relational and uses tables with a variable number of rows and a fixed number of columns that have to be 
+defined on the creation of the table. The tables and columns model the data of the kind of object to be stored. SQL databases
+focuses on storing the data efficiently and avoiding duplication.
+
+NoSQL databases are instead document-oriented and key-value databases which has an advantage in querying speed at the expense of 
+duplicate entries.
 https://www.xplenty.com/blog/the-sql-vs-nosql-difference/#:~:text=SQL%20databases%20are%20vertically%20scalable,data%20like%20documents%20or%20JSON.
--- hello.py
+++ hello.py
@@ -1,15 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+import re
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "SECRET_KEY"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, validators
+# from wtforms.validators import Required
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', [validators.DataRequired("Please enter your name.")])
+    email = StringField('What is your UofT email address?', [
+        # validators.DataRequired("Please enter your email address."),
+        validators.Email("Please enter a valid email address."),
+        # validators.Regexp("utoronto.ca", message="Please use a utoronto.ca address")
+    ])
+    submit = SubmitField('Submit')
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        form.name.data = ''
+        form.email.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), valid_email=("utoronto" in session.get('email')))
 
 
 @app.route('/user/<name>')
